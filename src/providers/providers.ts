608cb4d1c4be6931c667d43c6714f59c6398418b
mrfrogcoder <van@imfreemobile.com>
2017-10-10 15:53:28 +0800

    Update dev_van

--- src/providers/providers.ts
+++ src/providers/providers.ts
@@ -1,13 +1,6 @@
 import { AppProvider } from './app/app';
 import { EvtProvider } from './evt/evt';
 
-@NgModule({
-    providers: [
-        // Services
-        AppProvider,
-        EvtProvider
-    ]
-})
 export {
     AppProvider,
     EvtProvider
