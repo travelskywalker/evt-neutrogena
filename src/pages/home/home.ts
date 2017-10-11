677da516b01efce8cc1d23e097ea091522c274ae
Ray Rommel Santos <ray.santos@engmntprty.com>
2017-10-11 13:25:30 +0800

    initial commit - home/scan page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,8 +8,18 @@ import { AppProvider, EvtProvider } from '../../providers/providers';
 })
 export class HomePage {
 
+	links: any = [];
   constructor(public navCtrl: NavController, public appProvider: AppProvider, public evtProvider: EvtProvider) {
     console.log(appProvider,evtProvider);
+  }
+
+  ngOnInit(){
+  	this.links = [
+  					{text:"Privacy Policy",href:"javascript:void()"},
+  					{text:"Cookie Policy",href:"javascript:void()"},
+  					{text:"Legal Notice",href:"javascript:void()"},
+  					{text:"Modern Slavery Act Statement",href:"javascript:void()"},
+  					];
   }
 
 }
