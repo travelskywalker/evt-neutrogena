8e86f276f12f4745bd5799cc279d1019703588e1
Rex Mupas <rexmupas@IMFREE-REX-2.local>
2017-11-17 09:51:07 +0800

    build: change strategy on auth0 access

--- www/build/main.js
+++ www/build/main.js
@@ -3756,15 +3756,13 @@ var Config = {
     production: false,
     evt_app: "UNFlt65VdIwXzmNbndPc7LBKTPoQ68ROhq0nTdvXieu66esipebEBNXdL3NpcsMfbon8xOfhppVS60IN",
     evt_base_url: "https://api-eu.evrythng.com",
-    //evt_operator: "87NneGCimPPb5eZTr6nKPudKMhKzkGiqf8mvCF2N1hY2R3nOQA3sijF8KOkiZCdV9KTUpeZ3PkGacWQG",
-    //evt_operator_apikey: "hohzaKH7VbVp659Pnr5m3xg2DpKBivg9rFh6PttT5AnBtEn3s17B8OPAOpBjNTWdoRlosLTxJmUrpjTi",
     projectId: "U3NNqQhUe6s7QraRakG4fpNc",
     appId: "UG7N6Q2yVMsrtNRaak34fprg",
     auth0: {
-        clientID: 'w9VzQPQgiQaldMl3MaGkfEph63N6VJhi',
-        domain: 'demo-evt.eu.auth0.com',
+        clientID: '67JAI87YgeGyMWDBKZHedJ9LccRcjBC7',
+        domain: 'evtjandj.eu.auth0.com',
         responseType: 'token id_token',
-        audience: 'https://demo-evt.eu.auth0.com/userinfo',
+        audience: 'https://evtjandj.eu.auth0.com/userinfo',
         redirectUri: window.location.origin,
         scope: 'openid'
     },
@@ -3813,7 +3811,7 @@ var Config = {
         name: "www.neutrogena.co.uk",
         target: "_blank"
     },
-    lessonCompletionTimeLimit: 2,
+    lessonCompletionTimeLimit: 60 * 10,
     totalDailyLessonLimit: 30,
     courseDailyLessonLimit: 10,
     anonUserLessonLimit: 1,
