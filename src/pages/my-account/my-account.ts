4648e8bbf0c376a42a6e8ef92ad30dfcccbba133
Rex Mupas <rexmupas@IMFREE-REX-2.local>
2017-11-18 12:40:22 +0800

    improve my acount ux

--- src/pages/my-account/my-account.ts
+++ src/pages/my-account/my-account.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController} from 'ionic-angular';
 import { AuthService } from "../../providers/auth/auth.service";
 
 import { Validators, FormBuilder, FormGroup, FormControl } from "@angular/forms";
@@ -14,28 +14,29 @@ import { DeleteAccountPage } from '../delete-account/delete-account';
   templateUrl: 'my-account.html',
 })
 export class MyAccountPage {
-	FBreg : boolean = false;
-	private formGroup : FormGroup;
-  constructor(	public navCtrl: NavController,
-  				public navParams: NavParams,
-  				// private render: Renderer2,
-  				private auth0: AuthService,
-  				private formBuilder: FormBuilder) {
+  FBreg:boolean = false;
+  private formGroup:FormGroup;
+
+  constructor(public navCtrl:NavController,
+              public navParams:NavParams,
+              private auth0:AuthService,
+              private formBuilder:FormBuilder,
+              private loader:LoadingController) {
   }
 
-  ngOnInit(){
+  ngOnInit() {
 
-  	this.FBreg = this.auth0.isFB();
-  	console.log(this.FBreg);
-  	let usr = this.auth0.getUserDetailsFromStorage();
-  	let usrFName = this.FBreg ? usr['given_name'] : usr['user_metadata'].firstName;
-  	let usrLName = this.FBreg ? usr['family_name'] : usr['user_metadata'].lastName;
-  	this.formGroup = this.formBuilder.group({
-  		firstName: [usrFName, Validators.required],
-  		lastName: [usrLName, Validators.required],
-  		email: new FormControl({value:usr['email'],disabled:!this.FBreg}, Validators.required),
-  		password: ['', Validators.compose([Validators.minLength(8),Validators.required])]
-  	});
+    this.FBreg = this.auth0.isFB();
+    console.log(this.FBreg);
+    let usr = this.auth0.getUserDetailsFromStorage();
+    let usrFName = this.FBreg ? usr['given_name'] : usr['user_metadata'].firstName;
+    let usrLName = this.FBreg ? usr['family_name'] : usr['user_metadata'].lastName;
+    this.formGroup = this.formBuilder.group({
+      firstName: [usrFName, Validators.required],
+      lastName: [usrLName, Validators.required],
+      email: new FormControl({value: usr['email'], disabled: !this.FBreg}, Validators.required),
+      password: ['', Validators.compose([Validators.minLength(8), Validators.required])]
+    });
 
   }
 
@@ -43,46 +44,57 @@ export class MyAccountPage {
     console.log('ionViewDidLoad SignUpPage');
   }
 
-  focused(event){
-  	let node = event._elementRef.nativeElement;
+  focused(event) {
+    let node = event._elementRef.nativeElement;
   }
 
-  blurred(event){
-  	let node = event._elementRef.nativeElement;
+  blurred(event) {
+    let node = event._elementRef.nativeElement;
   }
 
-  dismissThis(){
-  	// should pop this page and then go to previous
-  	console.log("dismiss");
-  	if(this.navCtrl.canGoBack()){
-  		this.navCtrl.pop();
-  	}
-  	else{
-  		this.navCtrl.setRoot(HomePage);
-  	}
+  dismissThis() {
+    // should pop this page and then go to previous
+    console.log("dismiss");
+    if (this.navCtrl.canGoBack()) {
+      this.navCtrl.pop();
+    }
+    else {
+      this.navCtrl.setRoot(HomePage);
+    }
   }
 
-  logout(){
-  	this.auth0.logout();
+  logout() {
+    this.auth0.logout();
   }
 
-  updateUser(){
-  	let usr = this.formGroup.value;
-  	let self = this;
-  	this.auth0.updateUser({firstName:usr.firstName,lastName:usr.lastName}).then(res=>{
-        self.auth0.setUserMetadata(res['user_metadata']);
-        self.navCtrl.setRoot(HomePage);
-  	})
-  	.catch(err=>{
-  		console.log(err);
-  	});
+  updateUser() {
+    let usr = this.formGroup.value;
+    let self = this;
+
+    let load = this.loader.create({
+      spinner: 'crescent',
+      dismissOnPageChange: true,
+      showBackdrop: true,
+      content: `Please wait...`,
+      enableBackdropDismiss: true
+    });
+    load.present();
+
+    this.auth0.updateUser({firstName: usr.firstName, lastName: usr.lastName}).then(res=> {
+      self.auth0.setUserMetadata(res['user_metadata']);
+      load.dismissAll();
+    })
+    .catch(err=> {
+      load.dismissAll();
+      console.log(err);
+    });
   }
 
-  deleteAccount(){
-  	this.navCtrl.setRoot(DeleteAccountPage);
+  deleteAccount() {
+    this.navCtrl.setRoot(DeleteAccountPage);
   }
 
-  toFB(){
-  	window.location.href = "//facebook.com";
+  toFB() {
+    window.location.href = "//facebook.com";
   }
 }
