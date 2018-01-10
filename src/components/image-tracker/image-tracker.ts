c959054c65feff0462c009e43b4a1798104261cf
Ray Rommel Santos <ray.santos@engmntprty.com>
2018-01-10 14:23:14 +0800

    try canvas images for upload

--- src/components/image-tracker/image-tracker.ts
+++ src/components/image-tracker/image-tracker.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
-import {Slides} from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Slides, LoadingController } from 'ionic-angular';
 
 
 var sha1 = require('sha1');
@@ -15,12 +15,13 @@ var sha1 = require('sha1');
 })
 export class ImageTrackerComponent {
 
-	cloudName:string = 'demoengmntprty';
+	//cloudName:string = 'demoengmntprty';
+	cloudName:string = 'jandjstaging';
   text: string;
   photoProg: Array<any> = [];
   @ViewChild('Slides') slider: Slides;
 
-  constructor() {
+  constructor(private loader: LoadingController) {
     console.log('Hello ImageTrackerComponent Component');
     this.text = 'Hello World';
   }
@@ -72,6 +73,13 @@ export class ImageTrackerComponent {
 
   // *********** Upload file to Cloudinary ******************** //
 	uploadFile(file) {
+    let load = this.loader.create({
+      spinner: 'crescent',
+      dismissOnPageChange: true,
+      showBackdrop: true,
+      content: `Please wait...`,
+      enableBackdropDismiss:true});
+    load.present();
 		console.log(file);
 		let self = this;
   var url = `https://api.cloudinary.com/v1_1/${self.cloudName}/image/upload`;
@@ -89,38 +97,74 @@ export class ImageTrackerComponent {
   data.total: ${e.total}`);
   });*/
 
-  xhr.onreadystatechange = function(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      // File uploaded successfully
-      var response = JSON.parse(xhr.responseText);
-      // https://res.cloudinary.com/cloudName/image/upload/v1483481128/public_id.jpg
-      /*var url = response.secure_url;
-      // Create a thumbnail of the uploaded image, with 150px width
-      var tokens = url.split('/');
-      tokens.splice(-2, 0, 'w_150,c_scale');
-      var img = new Image(); // HTML5 Constructor
-      img.src = tokens.join('/');
-      img.alt = response.public_id;
-      document.getElementById('gallery').appendChild(img);*/
-      console.log(response);
-      self.photoProg.push(response.secure_url);
-      self.unlockSlides();
-      setTimeout(()=>{
-	  	self.slider.slideTo(self.photoProg.length+1);
-	  },500);
-    }
-  };
+      let rdr = new FileReader(); 
+      rdr.onloadend = function(){
+	    var tempImg = new Image();
+	    tempImg.src = rdr.result;
+	    tempImg.onload = function() {
+	      	var MAX_WIDTH = 400;
+	        var MAX_HEIGHT = 300;
+	        var tempW = tempImg.width;
+	        var tempH = tempImg.height;
+	        if (tempW > tempH) {
+	            if (tempW > MAX_WIDTH) {
+	               tempH *= MAX_WIDTH / tempW;
+	               tempW = MAX_WIDTH;
+	            }
+	        } else {
+	            if (tempH > MAX_HEIGHT) {
+	               tempW *= MAX_HEIGHT / tempH;
+	               tempH = MAX_HEIGHT;
+	            }
+	        }
+	 
+	        var canvas = document.createElement('canvas');
+	        canvas.width = tempW;
+	        canvas.height = tempH;
+	        var ctx = canvas.getContext("2d");
+	        ctx.drawImage(tempImg, 0, 0, tempW, tempH);
+	        var dataURL = canvas.toDataURL("image/jpeg");
+	        //var data = 'image='+dataURL;
 
-  let tstamp = Date.now().toString();
-  let pid = file.name+"_"+self.photoProg.length;
-  fd.append('file', file);
-  fd.append('public_id', pid);
-  fd.append('timestamp', tstamp);
-  fd.append('api_key', '299675785887213'); // Optional - add tag for image admin in Cloudinary
+		  xhr.onreadystatechange = function(e) {
+		    if (xhr.readyState == 4 && xhr.status == 200) {
+		      // File uploaded successfully
+		      var response = JSON.parse(xhr.responseText);
+		      // https://res.cloudinary.com/cloudName/image/upload/v1483481128/public_id.jpg
+		      /*var url = response.secure_url;
+		      // Create a thumbnail of the uploaded image, with 150px width
+		      var tokens = url.split('/');
+		      tokens.splice(-2, 0, 'w_150,c_scale');
+		      var img = new Image(); // HTML5 Constructor
+		      img.src = tokens.join('/');
+		      img.alt = response.public_id;
+		      document.getElementById('gallery').appendChild(img);*/
+		      console.log(response);
+		      self.photoProg.push(response.secure_url);
+		      self.unlockSlides();
+		      setTimeout(()=>{
+			  	self.slider.slideTo(self.photoProg.length+1);
+			  },500);
+		    	load.dismiss();
+		    }
+		  }
 
-  let signed = sha1('public_id='+pid+'&timestamp='+tstamp+"BBImHLi3cw-Y_NynlbMU3HYyhH0");
-  fd.append('signature', signed); // Optional - add tag for image admin in Cloudinary
-  xhr.send(fd);
+		  let tstamp = Date.now().toString();
+		  let pid = file.name+"_"+self.photoProg.length;
+		  let secret = "uJkQIneMpHgAJkqho1NLFroqGUg"; //"BBImHLi3cw-Y_NynlbMU3HYyhH0"
+		  fd.append('file', dataURL);
+		  fd.append('public_id', pid);
+		  fd.append('timestamp', tstamp);
+		  fd.append('api_key', '572517737342669'); // 299675785887213 Optional - add tag for image admin in Cloudinary
+
+		  let signed = sha1('public_id='+pid+'&timestamp='+tstamp+secret);
+		  fd.append('signature', signed); // Optional - add tag for image admin in Cloudinary
+		  xhr.send(fd);
+
+	      }
+  	}
+
+      rdr.readAsDataURL(file);
 
 
 }
