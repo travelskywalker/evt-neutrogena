2ddc37359574327b81ab90ec37e8434dbc5b0522
Ray Rommel Santos <ray.santos@engmntprty.com>
2018-01-11 14:29:43 +0800

    +3

--- src/components/image-tracker/image-tracker.ts
+++ src/components/image-tracker/image-tracker.ts
@@ -190,7 +190,6 @@ exifProc(orientation,ctx,canvas,exifImg){
 	        var ctx = canvas.getContext("2d");
   ctx.translate(exifImg.width * 0.5, exifImg.height * 0.5);
 */
-	ctx.translate(exifImg.width * 0.5, exifImg.height * 0.5);
   switch(orientation) {
     case 2:
         // horizontal flip
@@ -226,7 +225,7 @@ exifProc(orientation,ctx,canvas,exifImg){
     case 8:
         // 90° rotate left
         ctx.rotate(-0.5 * Math.PI);
-        ctx.translate(-canvas.width, 0);
+        //ctx.translate(-canvas.width, 0);
         break;
   }
 
@@ -234,8 +233,8 @@ exifProc(orientation,ctx,canvas,exifImg){
   	ctx.drawImage(exifImg, 0, 0, canvas.width, canvas.height);
   }
   else{
-	  ctx.translate(-canvas.width * 0.5, -canvas.height * 0.5);
-  		ctx.drawImage(exifImg, 0, 0, canvas.width, canvas.height);
+	 // ctx.translate(-canvas.width * 0.5, -canvas.height * 0.5);
+  		ctx.drawImage(exifImg, 0, 0, canvas.height, canvas.width);
   }
   return canvas.toDataURL("image/png");
 }
